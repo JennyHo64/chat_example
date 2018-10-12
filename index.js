ade9a84be0c97b9af42f1b8789cc3fdb7e0b64df
JennyHo64 <je64yyy@gmail.com>
2018-10-12 18:33:52 +0800

    181012_m

--- index.js
+++ index.js
@@ -2,60 +2,58 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/index', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){     //當新使用者連接進來的時候  io.on
-  console.log('a user connected: '+socket.id);
+app.get('/addtoRoom', function(req, res){
+  res.sendFile(__dirname + '/addtoRoom.html');
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', (socket) => {
-  let chatroom =  socket.id;//{}'room: '+
-    socket.join(chatroom , () => {  //.join 加入到指定的room
+function chatRoom(socket){
+  console.log('a user connected: '+ socket.id);   
+    socket.join(socket.id , function() {  //.join 加入到指定的room (以user id為room name)
       var rooms = Object.keys(socket.rooms);
       socket.on('chat message', function(msg){
-        socket.emit('chat message', msg);  //要用socket就會將訊息傳到個別的room，若用io就會將訊息傳給所有在連線上的人
-        console.log(rooms); // [ <socket.id>, 'room 237' ]
+        socket.emit('show message', msg);  //要用socket就會將訊息傳到個別的room，若用io就會將訊息傳給所有在連線上的人
+        console.log("room is: "+rooms);
         console.log('message: ' + msg);
-        // io.emit('a new user has joined the room'); // broadcast to everyone in the room
-        // io.sockets.to(rooms).emit('a new user has joined the room'); // broadcast to everyone in the room
+            });
       });
-      });
-
       //user left 
       socket.on('disconnect',function(){
-      console.log("The User : "+socket.id+" left.");
-      io.emit('user left');
-      // io.emit('user left',{
-      //   username:socket.username
-      // });
-    });
+        console.log("The User : "+socket.id+" left.");
+        io.emit('user left');
+      });
+
+      //b頁面 add to chatroom
+      socket.on('add to chatroom',function(roomAll){
+        console.log(roomAll);
+        socket.join(roomAll.roomId_key , function() { 
+          var rooms = Object.keys(socket.rooms);
+          //
+          // socket.on('chat message_b', function(msg){
+          //   socket.emit('show message_b', msg);
+          //
+        // let room = JSON.parse(roomAll);  //字串轉物件
+        // var obj_str = JSON.stringify(obj)  //物件轉字串
+        console.log('join into room:'+roomAll.roomId_key);
+      })
+    })
+    
       
-    });
-
-
+      //
+      socket.on('chat message_b', function(msg){
+        // socket.join(roomAll.roomId_key, function() {
+        io.emit('show message_b', msg);  //要用socket就會將訊息傳到個別的room，若用io就會將訊息傳給所有在連線上的人
+        })
+        // console.log("room is: "+rooms);
+        // console.log('message: ' + msg);
   
-  
-// /*-----------------------------------
-// //html範例
+}   
 
-// var app = require('express')();
-// var http = require('http').Server(app);
-
-// // app.get('/', function(req, res){
-// //   res.send('<h1>Hello world</h1>');
-// // });
-
-// app.get('/', function(req, res){
-//     res.sendFile(__dirname + '/index.html');
-//   });
-
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
-// --------------------------------------*/
+io.on('connection', chatRoom);          //當新使用者連接進來的時候  io.on
